3face02775fa4747507ca6c60b4131dc12a17bc2
Choi-younghoon <chldudgns222@naver.com>
2020-06-24 17:08:40 +0900

    커밋

--- src/client/Dao/crm_HospitalDao.java
+++ src/client/Dao/crm_HospitalDao.java
@@ -20,10 +20,11 @@ public class crm_HospitalDao {
 		sqlSes = sqlMapper.openSession();
 	}
 	public List<Map<String, Object>> hospitalList(Map<String, Object> pMap) {
+		//제휴병원 리스트
 		logger.info("[crm_HospitalDao] hospitalList호출 성공");
 		List<Map<String, Object>> hospitalList = null;
 		pMap.put("hp_open", "09:00");
-		pMap.put("hp_close", "18:00");
+//		pMap.put("hp_close", "18:00");
 		hospitalList = sqlSes.selectList("hospitalList", pMap);
 		logger.info("hospitalList: " + hospitalList.size()); //널포인트 인셉션 터지는 위치.
 		return hospitalList;
