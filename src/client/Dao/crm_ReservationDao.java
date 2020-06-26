e55917cd81f4a7560756f01bf7a9bd11c8777acb
Choi-younghoon <chldudgns222@naver.com>
2020-06-26 10:26:37 +0900

    예약 리스트 찐 완성

--- src/client/Dao/crm_ReservationDao.java
+++ src/client/Dao/crm_ReservationDao.java
@@ -22,17 +22,18 @@ public class crm_ReservationDao {
 		sqlSes = sqlMapper.openSession();
 	}
 
-	public List<Map<String, Object>> reservationList(Map<String, Object> pMap) {
+	public List<Map<String, Object>> proc_reservelist(Map<String, Object> pMap) {
 		//병원만 선택후  모든과, 의사T, 대기시간 찾기 or 진료과목 및 의사T 조건검색 으로 목록 확인
-		logger.info("[crm_ReservationDao] reservationList 호출성공");
-		List<Map<String, Object>> reservationList = null;
-		pMap.put("u_dept_name","소아과");
+		logger.info("[crm_ReservationDao] proc_reservelist 호출성공");
+		List<Map<String, Object>> proc_reservelist = null;
 		pMap.put("u_hp_code","647HP");
+		pMap.put("u_dept_name","소아과");
+		pMap.put("u_doc_name","조하윤");
 		pMap.put("fnum",1);
 		pMap.put("enum",5);
-		sqlSes.selectList("reservationList", pMap);
-		reservationList = (List<Map<String, Object>>)pMap.get("res_list");
-		return reservationList;
+		sqlSes.selectList("proc_reservelist", pMap);
+		proc_reservelist = (List<Map<String, Object>>)pMap.get("res_list");
+		return proc_reservelist;
 	}
 	
 	public List<Map<String, Object>> noticeList(Map<String, Object> pMap) {
@@ -91,9 +92,11 @@ public class crm_ReservationDao {
 	public static void main(String[] args) {
 		crm_ReservationDao res = new crm_ReservationDao();
 		Map<String, Object> pMap = new HashMap<String, Object>();
-		List<Map<String, Object>> list = res.reservationList(pMap);
+		List<Map<String, Object>> list = res.proc_reservelist(pMap);
 //		res.waitCheck(pMap);
 //		res.docSel(pMap);
 		System.out.println(list);
+		System.out.println(list.size());
+		
 	}
 }
